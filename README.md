e469374db3d2126b223b3a5575d067b2c52ae85b
Gabriela Rojas <grojas9807@gmail.com>
2024-05-30 17:47:19 -0500

    Upload solution to 2 logic exercise generation

---
+++ README.md
@@ -0,0 +1,4 @@
+# SA-LPD2 - PRÁCTICA DE CONVERSIONES DE TEMPERATURAS.
+## Ejercicio lógica - Bootcamp Generation. 
+
+Pendiente imprimir resultado por DOM, responsible mobile y diseño botón. 
--- app.js
+++ app.js
@@ -1,41 +1,67 @@
-// SA-LPD2 - PRÁCTICA DE CONVERSIONES DE TEMPERATURAS
-// Crear un programa en Javascript que realice lo siguiente:
-// Debe solicitar al usuario la temperatura en grados Celsius por prompt o por un input.
-// Debe convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
-// Debe imprimir ambos resultados por consola o por el DOM.
-// Debe ser capaz de identificar si los datos de entrada sean de tipo number, en caso contrario debe mandar un mensaje de error y volver a solicitar los datos.
-// Prueba tu programa con las siguientes entradas para asegurarte que funcione correctamente:
-// Entrada: 
-// 45.
-// Salida
-// Grados Kelvin: 318.15
-// Grados Fahrenheit: 113. 
-// Entrada:
-//  14.
-// Salida
-// Grados Kelvin: 287.15
-// Grados Fahrenheit: 57.2. 
+//Defino mis constantes globales
+const formConvertir = document.getElementById('form')
+const errorMessage = document.getElementById('error-message')
 
+//Escuchar evento submit form
+formConvertir.addEventListener('submit', (e) => {
+  //Evitar que el formulario se envíe, en este caso no es necesario. 
+  e.preventDefault();
 
-// let gradosCelsius;
-// gradosCelsius = parseFloat(prompt("Ingrese los grados que quiere convertir"));
+  //Capturar valor temperatura y unidad desde donde se desea convertir
+  const inputTemperature = document.getElementById('temperature-value').value;
+  const unitTemperature = document.getElementById('type-temperature').value;
 
-// while (!isFinite(gradosCelsius)){
-//   gradosCelsius = Number(prompt("Ingrese un dato válido de los grados que quiere convertir"));
-// }
+  //Convertir input de termperatura en valor númerico.
+  let gradosAConvertir = parseFloat(inputTemperature);
+  let celsius, fahrenheit, kelvin
 
-if( typeof(gradosCelsius) == 'number' && isFinite(gradosCelsius) ){
-  convertFahrenheit = (gradosCelsius) => {
-    return (gradosCelsius * 9/5) + 32;
+  // Verifico que el input de temperatura si sea un número si no, imprimo mensaje de error por temperatura inválida
+  if(typeof(gradosAConvertir) == 'number' && isFinite(gradosAConvertir) ){
+    errorMessage.classList.add("hide")
+    errorMessage.classList.remove("show")
+    switch(unitTemperature){
+      case 'celsius':
+        celsius = gradosAConvertir;
+        fahrenheit = (gradosAConvertir * 9/5) + 32;
+        kelvin = gradosAConvertir + 273.15;
+        break;
+      case 'fahrenheit':
+        celsius = (gradosAConvertir - 32) * 5/9;
+        fahrenheit = gradosAConvertir;
+        kelvin = celsius + 273.15;
+        break;
+      case 'kelvin':
+        celsius = gradosAConvertir - 273;
+        fahrenheit = (gradosAConvertir - 273) * 9/5;
+        kelvin = gradosAConvertir;
+        break;
+
+      //imprimo mensaje de error por unidad no escogida y me salgo de la función
+      default: 
+        errorMessage.innerText = `Ups 😶! \n Por favor selecciona al menos una unidad`
+        errorMessage.classList.add("show")
+        errorMessage.classList.remove("hide")
+        return
+    }
+  } 
+
+  //imprimo mensaje de error por temperatura errónea y me salgo de la función
+  else{
+    errorMessage.innerText = `Ups 😶! \n Por favor escribe nuevamente un valor válido para temperatura`
+    errorMessage.classList.add("show")
+    errorMessage.classList.remove("hide")
+    return
   }
-  convertKelvin = (gradosCelsius) => {
-    return gradosCelsius + 273.15;
-  }
-}
 
-console.log(convertFahrenheit(gradosCelsius));
-console.log(convertKelvin(gradosCelsius));
+  console.log(`${celsius}, ${fahrenheit}, ${kelvin}`);
+  console.log(typeof(gradosAConvertir));
+  console.log(unitTemperature);
+})
 
-console.log(typeof(gradosCelsius));
+
+
+
+
+
 
 
